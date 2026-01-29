44e4a6831803e767d05c36791916a0b88ff53bb7
nafhansa <nafhan1723@gmail.com>
2026-01-30 02:02:29 +0700

    fix: extend CSP whitelist to include vercel.live and grainy-gradients

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,12 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://core.spreedly.com https://global.localizecdn.com https://js.stripe.com https://applepay.cdn-apple.com https://cdn.paddle.com https://sandbox-cdn.paddle.com https://effectivelyassets.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              font-src 'self' https://fonts.gstatic.com;
-              img-src 'self' data: https://cdn.paddle.com https://sandbox-cdn.paddle.com https://*.googleusercontent.com;
-              connect-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://checkout-service.paddle.com https://sandbox-checkout-service.paddle.com https://*.googleapis.com https://*.firebaseio.com https://*.firebase.com;
-              frame-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://core.spreedly.com https://global.localizecdn.com https://js.stripe.com https://applepay.cdn-apple.com https://cdn.paddle.com https://sandbox-cdn.paddle.com https://effectivelyassets.com https://vercel.live;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://vercel.live;
+              font-src 'self' https://fonts.gstatic.com https://vercel.live;
+              img-src 'self' data: https://cdn.paddle.com https://sandbox-cdn.paddle.com https://*.googleusercontent.com https://grainy-gradients.vercel.app https://vercel.live;
+              connect-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://checkout-service.paddle.com https://sandbox-checkout-service.paddle.com https://*.googleapis.com https://*.firebaseio.com https://*.firebase.com https://vercel.live;
+              frame-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://vercel.live;
               object-src 'none';
               base-uri 'self';
               form-action 'self';
