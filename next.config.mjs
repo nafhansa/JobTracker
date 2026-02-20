f276678ea685b8bb9ec2af309595bf94bdca6f77
nafhansa <nafhan1723@gmail.com>
2026-02-20 15:11:18 +0700

    fix: removed all related logic to ads

--- next.config.mjs
+++ next.config.mjs
@@ -11,12 +11,12 @@ const nextConfig = {
                         key: 'Content-Security-Policy',
                         value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://core.spreedly.com https://global.localizecdn.com https://js.stripe.com https://applepay.cdn-apple.com https://cdn.paddle.com https://sandbox-cdn.paddle.com https://effectivelyassets.com https://vercel.live https://*.effectivegatecpm.com https://apis.google.com https://accounts.google.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://core.spreedly.com https://global.localizecdn.com https://js.stripe.com https://applepay.cdn-apple.com https://cdn.paddle.com https://sandbox-cdn.paddle.com https://effectivelyassets.com https://vercel.live https://apis.google.com https://accounts.google.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://vercel.live https://cdn.paddle.com https://sandbox-cdn.paddle.com;
               font-src 'self' https://fonts.gstatic.com https://vercel.live;
-              img-src 'self' data: https://cdn.paddle.com https://sandbox-cdn.paddle.com https://*.googleusercontent.com https://grainy-gradients.vercel.app https://vercel.live https://fizzyacerbitymellow.com https://pacestrengthentribute.com;
-              connect-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://checkout-service.paddle.com https://sandbox-checkout-service.paddle.com https://*.googleapis.com https://*.firebaseio.com https://*.firebase.com https://vercel.live https://*.effectivegatecpm.com https://tallthirsty.com https://fizzyacerbitymellow.com https://accounts.google.com https://www.googleapis.com https://*.supabase.co wss://*.supabase.co https://pacestrengthentribute.com;
-              frame-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://vercel.live https://*.effectivegatecpm.com https://tallthirsty.com https://accounts.google.com https://*.firebaseapp.com;
+              img-src 'self' data: https://cdn.paddle.com https://sandbox-cdn.paddle.com https://*.googleusercontent.com https://grainy-gradients.vercel.app https://vercel.live;
+              connect-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://checkout-service.paddle.com https://sandbox-checkout-service.paddle.com https://*.googleapis.com https://*.firebaseio.com https://*.firebase.com https://vercel.live https://accounts.google.com https://www.googleapis.com https://*.supabase.co wss://*.supabase.co;
+              frame-src 'self' https://buy.paddle.com https://sandbox-buy.paddle.com https://vercel.live https://accounts.google.com https://*.firebaseapp.com;
               object-src 'none';
               base-uri 'self';
               form-action 'self';
